312d513891b74d3b27ac9a43485727c8423f08f3
Maneesha0726 <komaramaneesha@gmail.com>
2025-11-30 17:17:09 +0530

    urls update

--- Energyapp/urls.py
+++ Energyapp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .views import Index
+from . import views
 urlpatterns = [
     path("", Index.as_view(), name="index"),
+    path('analyze/', views.analyze_image, name='analyze_image')
+
 ]
